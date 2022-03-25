902c7df8b64f4cecdb024de307c1c0ff8052ea91
izaiahsims <102048652+izaiahsims@users.noreply.github.com>
2022-03-25 10:16:02 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+function askname() {
+    var name = prompt("what is your name");
+     return name
+    
+
+}
+function quarry() {
+    var tempture  = prompt (`what temp what you like to convert ${a} ?`)
+
+    return 
+}
+
+function convert() {
+    var convert = prompt("do you want Celcelcius to farenheit") 
+    if (that == "C");
+    else if (that == "F");
+}
+
+function mathe(b) {      
+    var C = ((b-32)*5/9);
+    return C
+}
+
+function mathq(c) {
+    var F = ((c*9)/5+32);
+    return F
+}
+
+function element() {
+    var a =choose()
+    if (a == "f") {
+     alert(conver(quarry(askname())))  
+    }else if (a== "c") {
+     alert(conver(quarry(askname())))
+    }else {
+     alert("exit")
+    }
+
+}
+
+element()
+
+// function ----()
